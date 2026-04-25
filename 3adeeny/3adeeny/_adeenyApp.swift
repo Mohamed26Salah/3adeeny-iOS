9ea27a348906ff2662133453c36f5afefcdf17b1
Mohamed26Salah <76624455+Mohamed26Salah@users.noreply.github.com>
2026-04-25 03:20:12 +0300

    init

---
+++ 3adeeny/3adeeny/_adeenyApp.swift
@@ -0,0 +1,17 @@
+//
+//  _adeenyApp.swift
+//  3adeeny
+//
+//  Created by Mohamed Salah on 25/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct _adeenyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 3adeeny/3adeenyTests/_adeenyTests.swift
@@ -0,0 +1,19 @@
+//
+//  _adeenyTests.swift
+//  3adeenyTests
+//
+//  Created by Mohamed Salah on 25/04/2026.
+//
+
+import Testing
+@testable import _adeeny
+
+struct _adeenyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
